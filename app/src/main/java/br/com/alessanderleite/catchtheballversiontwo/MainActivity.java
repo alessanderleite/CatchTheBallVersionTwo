b54187ded3137caa7c146e5161a01a3f7bde5adf
alessanderleite <alessander.logistica@gmail.com>
2019-02-15 22:34:53 -0200

    code the startGame method

--- app/src/main/java/br/com/alessanderleite/catchtheballversiontwo/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/catchtheballversiontwo/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,6 +65,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View view) {
+        start_flg = true;
+        startLayout.setVisibility(View.INVISIBLE);
+
+        if (frameHeight == 0) {
+            frameHeight = gameFrame.getHeight();
+            frameWidth = gameFrame.getWidth();
+            initialFrameWidth = frameWidth;
+
+            boxSize = box.getHeight();
+            boxX = box.getX();
+            boxY = box.getY();
+        }
+        box.setX(0.0f);
+        black.setY(3000.0f);
+        orange.setY(3000.0f);
+        pink.setY(3000.0f);
+
+        blackY = black.getY();
+        orangeY = orange.getY();
+        pinkY = pink.getY();
+
+        box.setVisibility(View.VISIBLE);
+        black.setVisibility(View.VISIBLE);
+        orange.setVisibility(View.VISIBLE);
+        pink.setVisibility(View.VISIBLE);
+
+        timeCount = 0;
+        score = 0;
+        scoreLabel.setText("Score : 0");
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+            }
+        }, 0, 20);
     }
 
     public void quitGame(View view) {
